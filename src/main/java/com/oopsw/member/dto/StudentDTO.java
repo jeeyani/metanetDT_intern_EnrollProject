4685a6e2f45960f6bbf6387ab1f558cc75ebb9fe
user <user@DESKTOP-F4T27T3>
2021-02-11 17:32:11 +0900

    studentDTO

--- src/main/java/com/oopsw/member/dto/StudentDTO.java
+++ src/main/java/com/oopsw/member/dto/StudentDTO.java
@@ -1,5 +1,83 @@
 package com.oopsw.member.dto;
 
 public class StudentDTO {
+	
+	//학생정보
+	private String studentId;
+	private String pw;
+	private String name;
+	private String phone;
+	private String gender;
+	private String email;
+	private String address;
+	private String state;
 
+	//학과명
+	private int deptmentNo;
+	private String deptName;
+	
+	
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getDeptmentNo() {
+		return deptmentNo;
+	}
+	public void setDeptmentNo(int deptmentNo) {
+		this.deptmentNo = deptmentNo;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	
+	
+	
 }
