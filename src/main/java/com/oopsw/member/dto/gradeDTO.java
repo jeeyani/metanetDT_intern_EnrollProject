3b74ee23604ddc882a286ba87eb214b0f193cf0e
cgKang <elien940318@gmail.com>
2021-02-11 17:10:47 +0900

    gradeDTO

--- src/main/java/com/oopsw/member/dto/gradeDTO.java
+++ src/main/java/com/oopsw/member/dto/gradeDTO.java
@@ -1,6 +1,64 @@
 package com.oopsw.member.dto;
 
-public class gradeDTO {
+public class gradeDTO {	
+	private int sujectNo;
+	private String subjGroup;
+	private String subjName;
+	private int subjScore;
+	private String deptName;
+	private String subjProfessor;
+	private int grade;
+	private String name;
+	
+	public int getSujectNo() {
+		return sujectNo;
+	}
+	public void setSujectNo(int sujectNo) {
+		this.sujectNo = sujectNo;
+	}
+	public String getSubjGroup() {
+		return subjGroup;
+	}
+	public void setSubjGroup(String subjGroup) {
+		this.subjGroup = subjGroup;
+	}
+	public String getSubjName() {
+		return subjName;
+	}
+	public void setSubjName(String subjName) {
+		this.subjName = subjName;
+	}
+	public int getSubjScore() {
+		return subjScore;
+	}
+	public void setSubjScore(int subjScore) {
+		this.subjScore = subjScore;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	public String getSubjProfessor() {
+		return subjProfessor;
+	}
+	public void setSubjProfessor(String subjProfessor) {
+		this.subjProfessor = subjProfessor;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	
 	
 }
